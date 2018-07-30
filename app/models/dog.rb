df169849064f38f9aeb5733d1ec42390f6df4e66
Kevin Chi <kevinchi118@gmail.com>
2018-07-30 20:55:22 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,10 @@
-class Dog 
+class Dog
   attr_accessor :name, :breed, :age
 
-  def initalize(name, breed, age)
-    @name = name 
-    @breed = breed 
-    @age = age 
-  end 
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 
-end 
+end
